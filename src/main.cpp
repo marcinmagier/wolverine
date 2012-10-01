099309285fcd9b94d876778a8fb7294d63a0bad5
Marcin Magier <marcinmagier@gmail.com>
2012-10-01 21:15:20 +0200

    mainwindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
-// main.cpp
 
 #include "AppConfig.h"
+#include <QApplication>
+#include <QMainWindow>
 
 
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
+    QApplication app(argc, argv);
+    app.setApplicationName("Wolverine");
 
     AppConfig *settings = AppConfig::instance();
-
     settings->loadConfiguration();
     settings->saveConfiguration();
 
-    return 0;
+    QMainWindow mainWin;
+    mainWin.resize(800, 600);
+    mainWin.show();
+    return app.exec();
 }
