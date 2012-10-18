666137614c497b0102f007e3b2d585c48b6e408a
Marcin Magier <marcinmagier@gmail.com>
2012-10-18 08:25:32 +0200

    dropEvent() is ready

--- extlib/qtmanagedtoolbar/qtmanagedtoolbarlistbox.cpp
+++ extlib/qtmanagedtoolbar/qtmanagedtoolbarlistbox.cpp
@@ -1,5 +1,9 @@
 #include "qtmanagedtoolbarlistbox.h"
 
+#include <QDropEvent>
+#include <QDebug>
+#include <QByteArray>
+
 
 QtManagedToolbarListbox::QtManagedToolbarListbox(QWidget *parent) :
     QListWidget(parent)
@@ -10,5 +14,23 @@ QtManagedToolbarListbox::QtManagedToolbarListbox(QWidget *parent) :
 
 void QtManagedToolbarListbox::dropEvent(QDropEvent *event)
 {
-    QListWidget::dropEvent(event);
+    QByteArray encoded = event->mimeData()->data("application/x-qabstractitemmodeldatalist");
+    QDataStream stream(&encoded, QIODevice::ReadOnly);
+
+    while (!stream.atEnd())
+    {
+        int row, col;
+        QMap<int,  QVariant> roleDataMap;
+        stream >> row >> col >> roleDataMap;
+
+         //roleDataMap format - QMap((0, QVariant(QString, "ItemName") ) ( 1 ,  QVariant(QIcon, ) ) )
+        QString name = roleDataMap.value(0).toString();
+        if(name.isEmpty() || name == "Separator")
+            continue;
+
+        QList<QListWidgetItem *> listItems = findItems(name, Qt::MatchExactly);
+        foreach(QListWidgetItem *item, listItems) {
+            item->setHidden(false);
+        }
+    }
 }
