d4c97a24dc8829d9371a28c6d38b8012601d5cba
Marcin Magier <marcinmagier@gmail.com>
2012-12-19 00:04:43 +0100

    add tabbar settings

--- include/CfgGeneralSettings.h
+++ include/CfgGeneralSettings.h
@@ -9,28 +9,49 @@ class GeneralSettings : public QObject
 {
     Q_OBJECT
 
-    Q_PROPERTY(QString  language                READ getLanguage                WRITE setLanguage           )
+    Q_PROPERTY(QString  language                READ getLanguage                WRITE setLanguage               )
 
-    Q_PROPERTY(QString  logLevel                READ getLogLevel                WRITE setLogLevel           )
-    Q_PROPERTY(QString  logFilePath             READ getLogFilePath             WRITE setLogFilePath        )
-    Q_PROPERTY(bool     logFileEnabled          READ isLogFileEnabled           WRITE setLogFileEnabled     )
-    Q_PROPERTY(bool     logConsoleEnabled       READ isLogConsoleEnabled        WRITE setLogConsoleEnabled  )
+    Q_PROPERTY(QString  logLevel                READ getLogLevel                WRITE setLogLevel               )
+    Q_PROPERTY(QString  logFilePath             READ getLogFilePath             WRITE setLogFilePath            )
+    Q_PROPERTY(bool     logFileEnabled          READ isLogFileEnabled           WRITE setLogFileEnabled         )
+    Q_PROPERTY(bool     logConsoleEnabled       READ isLogConsoleEnabled        WRITE setLogConsoleEnabled      )
+
+    Q_PROPERTY(bool     tabBarHidden            READ isTabBarHidden             WRITE setTabBarHidden           )
+    Q_PROPERTY(bool     tabBarVertical          READ isTabBarVertical           WRITE setTabBarVertical         )
+    Q_PROPERTY(bool     tabBarLocked            READ isTabBarLocked             WRITE setTabBarLocked           )
+    Q_PROPERTY(bool     tabBarCloseVisible      READ isTabBarCloseVisible       WRITE setTabBarCloseVisible     )
+    Q_PROPERTY(bool     tabBarDoubleClkClose    READ isTabBarDoubleClkClose     WRITE setTabBarDoubleClkClose   )
+    Q_PROPERTY(bool     tabBarMiddleBtnClose    READ isTabBarMiddleBtnClose     WRITE setTabBarMiddleBtnClose   )
 
 
 public:
     explicit GeneralSettings();
 
-    QString getLanguage() { return mLanguage; }
 
-    QString getLogLevel() { return mLogLevel; }
-    QString getLogFilePath() { return mLogFilePath; }
-    bool isLogFileEnabled() { return mLogFileEnabled; }
-    bool isLogConsoleEnabled() { return mLogConsoleEnabled; }
+    QString getLanguage();
 
+    QString getLogLevel();
+    QString getLogFilePath();
+    bool isLogFileEnabled();
+    bool isLogConsoleEnabled();
+
+    bool isTabBarHidden();
+    bool isTabBarVertical();
+    bool isTabBarLocked();
+    bool isTabBarCloseVisible();
+    bool isTabBarDoubleClkClose();
+    bool isTabBarMiddleBtnClose();
 
 
 signals:
     void languageChanged(QString val);
+
+    void tabBarHiddenChanged(bool val);
+    void tabBarVerticalChanged(bool val);
+    void tabBarLockedChanged(bool val);
+    void tabBarCloseVisibleChanged(bool val);
+    void tabBarDoubleClkCloseChanged(bool val);
+    void tabBarMiddleBtnCloseChanged(bool val);
 
 
 public slots:
@@ -41,6 +62,12 @@ public slots:
     void setLogFileEnabled(bool val);
     void setLogConsoleEnabled(bool val);
 
+    void setTabBarHidden(bool val);
+    void setTabBarVertical(bool val);
+    void setTabBarLocked(bool val);
+    void setTabBarCloseVisible(bool val);
+    void setTabBarDoubleClkClose(bool val);
+    void setTabBarMiddleBtnClose(bool val);
 
 
 private:
@@ -50,6 +77,13 @@ private:
     QString mLogFilePath;
     bool mLogFileEnabled;
     bool mLogConsoleEnabled;
+
+    bool mTabBarHidden;
+    bool mTabBarVertical;
+    bool mTabBarLocked;
+    bool mTabBarCloseVisible;
+    bool mTabBarDoubleClkClose;
+    bool mTabBarMiddleBtnClose;
 
 };
 
--- src/settings/CfgGeneralSettings.cpp
+++ src/settings/CfgGeneralSettings.cpp
@@ -13,8 +13,17 @@ GeneralSettings::GeneralSettings()
     mLogFilePath = "Wolverine.log";
     mLogFileEnabled = true;
     mLogConsoleEnabled = true;
+
+    mTabBarHidden = false;
+    mTabBarVertical = false;
+    mTabBarLocked = false;
+    mTabBarCloseVisible = true;
+    mTabBarDoubleClkClose = true;
+    mTabBarMiddleBtnClose = true;
 }
 
+
+//=======================  Lang  ============================================//
 
 void GeneralSettings::setLanguage(QString val)
 {
@@ -23,26 +32,122 @@ void GeneralSettings::setLanguage(QString val)
         emit languageChanged(mLanguage);
     }
 }
+QString GeneralSettings::getLanguage()
+{
+    return mLanguage;
+}
 
+
+//=======================  Log  =============================================//
 
 void GeneralSettings::setLogLevel(QString val)
 {
     mLogLevel = val;
+}
+QString GeneralSettings::getLogLevel()
+{
+    return mLogLevel;
 }
 
 void GeneralSettings::setLogFilePath(QString val)
 {
     mLogFilePath = val;
 }
+QString GeneralSettings::getLogFilePath()
+{
+    return mLogFilePath;
+}
 
 void GeneralSettings::setLogFileEnabled(bool val)
 {
     mLogFileEnabled = val;
 }
+bool GeneralSettings::isLogFileEnabled()
+{
+    return mLogFileEnabled;
+}
 
 void GeneralSettings::setLogConsoleEnabled(bool val)
 {
     mLogConsoleEnabled = val;
+}
+bool GeneralSettings::isLogConsoleEnabled()
+{
+    return mLogConsoleEnabled;
+}
+
+//=======================  TabBar  ==========================================//
+
+void GeneralSettings::setTabBarHidden(bool val)
+{
+    if(mTabBarHidden != val) {
+        mTabBarHidden = val;
+        emit tabBarHiddenChanged(mTabBarHidden);
+    }
+}
+bool GeneralSettings::isTabBarHidden()
+{
+    return mTabBarHidden;
+}
+
+void GeneralSettings::setTabBarVertical(bool val)
+{
+    if(mTabBarVertical != val) {
+        mTabBarVertical = val;
+        emit tabBarVerticalChanged(mTabBarVertical);
+    }
+}
+bool GeneralSettings::isTabBarVertical()
+{
+    return mTabBarVertical;
+}
+
+void GeneralSettings::setTabBarLocked(bool val)
+{
+    if(mTabBarLocked != val) {
+        mTabBarLocked = val;
+        emit tabBarLockedChanged(mTabBarLocked);
+    }
+}
+bool GeneralSettings::isTabBarLocked()
+{
+    return mTabBarLocked;
+}
+
+void GeneralSettings::setTabBarCloseVisible(bool val)
+{
+    if(mTabBarCloseVisible != val) {
+        mTabBarCloseVisible = val;
+        emit tabBarCloseVisibleChanged(mTabBarCloseVisible);
+    }
+}
+bool GeneralSettings::isTabBarCloseVisible()
+{
+    return mTabBarCloseVisible;
+}
+
+void GeneralSettings::setTabBarDoubleClkClose(bool val)
+{
+    if(mTabBarDoubleClkClose != val) {
+        mTabBarDoubleClkClose = val;
+        emit tabBarDoubleClkCloseChanged(mTabBarDoubleClkClose);
+    }
+}
+bool GeneralSettings::isTabBarDoubleClkClose()
+{
+    return mTabBarDoubleClkClose;
+}
+
+void GeneralSettings::setTabBarMiddleBtnClose(bool val)
+{
+    if(mTabBarMiddleBtnClose != val) {
+        mTabBarMiddleBtnClose = val;
+        emit tabBarMiddleBtnCloseChanged(mTabBarMiddleBtnClose);
+    }
+}
+bool GeneralSettings::isTabBarMiddleBtnClose()
+{
+    return mTabBarMiddleBtnClose;
 }
 
 
