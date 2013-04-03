1c800ab7adb0a869396f12f3b73ba3713020fbe1
Marcin Magier <marcinmagier@gmail.com>
2013-04-03 20:42:45 +0200

    WFindReqWidget as stacked widget

--- src/finder/WFindReqWidget.cpp
+++ src/finder/WFindReqWidget.cpp
@@ -11,6 +11,16 @@ FindReqWidget::FindReqWidget(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->stackedWidget->setCurrentIndex(0);
+
+    ui->btnCaseSensitive->setIcon(QIcon(":/search_sensitive.png"));
+    ui->btnWholeWords->setIcon(QIcon(":/search_words.png"));
+    ui->btnRegexp->setIcon(QIcon(":/search_regexp.png"));
+    ui->btnReverseDirection->setIcon(QIcon(":/search_reverse.png"));
+    ui->btnWrap->setIcon(QIcon(":/search_wrap.png"));
+    ui->btnInSelection->setIcon(QIcon(":/search_selection.png"));
+    ui->btnMark->setIcon(QIcon(":/search_mark.png"));
+
 }
 
 FindReqWidget::~FindReqWidget()
