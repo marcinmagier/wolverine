aff8c15d60e474ecfc46def36d68c1733ed4ce45
Magier Marcin <marcinmagier@gmail.com>
2013-05-19 09:10:42 +0200

    Update add/remove menuitem.sh

--- resources/appicon/add-menuitem.sh
+++ resources/appicon/add-menuitem.sh
@@ -2,8 +2,8 @@
  
 APP_NAME=wolverine
 
-xdg-desktop-menu install $APP_NAME.desktop
+xdg-desktop-menu install --novendor $APP_NAME.desktop
 
-xdg-icon-resource install --size  16 $APP_NAME-16.png  $APP_NAME
-xdg-icon-resource install --size  32 $APP_NAME-32.png  $APP_NAME
-xdg-icon-resource install --size  48 $APP_NAME-48.png  $APP_NAME
+xdg-icon-resource install --novendor --size  16 $APP_NAME-16.png  $APP_NAME
+xdg-icon-resource install --novendor --size  32 $APP_NAME-32.png  $APP_NAME
+xdg-icon-resource install --novendor --size  48 $APP_NAME-48.png  $APP_NAME
